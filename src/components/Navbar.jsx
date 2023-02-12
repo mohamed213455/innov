8099a1c6e04ad15f9cd3e60075a478ffa4853875
mohamed213455 <mohamed.kalech@esprit.tn>
2023-02-12 20:16:56 +0100

    frist

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  const [active, setActive] = useState("Home");
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[145px] " />
+
+      
+
+      <ul className="list-none sm:flex hidden justify-center items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
+              active === nav.title ? "text-white" : "text-dimWhite"
+            } ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+            onClick={() => setActive(nav.title)}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle(!toggle)}
+        />
+
+        <div
+          className={`${
+            !toggle ? "hidden" : "flex"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex justify-end items-start flex-1 flex-col">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  active === nav.title ? "text-white" : "text-dimWhite"
+                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+            
+          </ul>
+        </div>
+        
+      </div>
+      
+
+      <div className="d-flex justify-between ">
+  
+
+  <button style={{
+    backgroundColor: "transparent", 
+    color: "white", 
+    borderRadius: "8px", 
+    border: "1px solid white", 
+    padding: "10px 60px",
+    
+  }}>Sign in</button>
+
+  <button style={{
+    backgroundColor: "#5DADEC", 
+    color: "white", 
+    borderRadius: "8px", 
+    border: "none", 
+    padding: "10px 60px",
+    marginLeft: 20
+  }}>Sign up</button>
+</div>
+
+    </nav>
+  );
+};
+
+export default Navbar;
